9cd1c953ec09cc02f42fde6a9e96e0fa3109d999
Sergio <sagg_301@outlook.com>
2020-04-30 18:46:13 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from '@material-ui/core/Button';
+import Album from './pages/Menu'
+
+function App() {
+  return (
+    <Album></Album>
+  );
+}
+
+export default App;
